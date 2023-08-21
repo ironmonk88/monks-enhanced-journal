1e944585bd6e3b77ab479005d924019b7d39e6fa
ironmonk88 <75920956+ironmonk88@users.noreply.github.com>
2023-08-21 09:39:52 -0700

    11.06 changes

--- monks-enhanced-journal.js
+++ monks-enhanced-journal.js
@@ -2471,9 +2471,15 @@ export class MonksEnhancedJournal {
         */
         if (!game.modules.get('monks-combat-details')?.active) {
             patchFunc("Draggable.prototype._onDragMouseUp", async function (wrapped, ...args) {
-                for (const cls of this.app.constructor._getInheritanceChain()) {
-                    Hooks.callAll(`dragEnd${cls.name}`, this.app, this.app.position);
-                }
+                try {
+                    if (this.app.constructor._getInheritanceChain) {
+                        for (const cls of this.app.constructor._getInheritanceChain()) {
+                            Hooks.callAll(`dragEnd${cls.name}`, this.app, this.app.position);
+                        }
+                    } else {
+                        Hooks.callAll(`dragEnd${this.app.constructor.name}`, this.app, this.app.position);
+                    }
+                } catch (e) { }
                 return wrapped(...args);
             });
         }
