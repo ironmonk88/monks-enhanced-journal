a616f6581eabeebd794cd77f10c423ebfdc8a308
ironmonk88 <75920956+ironmonk88@users.noreply.github.com>
2023-08-21 21:55:09 -0700

    11.07 changes

--- monks-enhanced-journal.js
+++ monks-enhanced-journal.js
@@ -78,13 +78,20 @@ export let isV11 = () => {
 };
 
 export let patchFunc = (prop, func, type = "WRAPPER") => {
-    if (game.modules.get("lib-wrapper")?.active) {
-        libWrapper.register("monks-enhanced-journal", prop, func, type);
-    } else {
+    let nonLibWrapper = () => {
         const oldFunc = eval(prop);
         eval(`${prop} = function (event) {
             return func.call(this, ${type != "OVERRIDE" ? "oldFunc.bind(this)," : ""} ...arguments);
         }`);
+    }
+    if (game.modules.get("lib-wrapper")?.active) {
+        try {
+            libWrapper.register("monks-enhanced-journal", prop, func, type);
+        } catch (e) {
+            nonLibWrapper();
+        }
+    } else {
+        nonLibWrapper();
     }
 }
 
